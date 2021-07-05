56dfd941f1552cdbb84f1b46fbc74833fe8f81a7
Ashwath V A <vaashwath@gmail.com>
2021-07-05 20:11:44 +0530

    Basic stacking of modifiers works. Have to concentrate on the scaling and offset

--- src/engraving/libmscore/undo.cpp
+++ src/engraving/libmscore/undo.cpp
@@ -1818,7 +1818,8 @@ void ChangeStyleVal::flip(EditData*)
         case Sid::chordQualityMinor:
         case Sid::chordQualityAugmented:
         case Sid::chordQualityDiminished:
-        case Sid::chordModifierOmit: {
+        case Sid::chordModifierOmit:
+        case Sid::stackModifiers: {
             score->chordList()->unload();
             qreal qmag = score->styleD(Sid::chordQualityMag);
             qreal qadjust = score->styleD(Sid::chordQualityAdjust);
@@ -1832,7 +1833,7 @@ void ChangeStyleVal::flip(EditData*)
             }
             score->chordList()->read(score->styleSt(Sid::chordDescriptionFile));
             score->chordList()->setCustomChordList(score->styleSt(Sid::chordStyle) == "custom");
-            score->setUpQualitySymbols();
+            score->updateChordList();
         }
         break;
         case Sid::spatium:
