048fe4cfdfa2665cb40b99aa4304c4b67e99de28
Ashwath V A <vaashwath@gmail.com>
2021-07-08 21:55:32 +0530

    Capitalize options + Parentheses options

--- src/engraving/libmscore/undo.cpp
+++ src/engraving/libmscore/undo.cpp
@@ -1812,6 +1812,12 @@ void ChangeStyleVal::flip(EditData*)
         case Sid::chordExtensionAdjust:
         case Sid::chordModifierMag:
         case Sid::chordModifierAdjust:
+        case Sid::chordAlterationsParentheses:
+        case Sid::chordSuspensionsParentheses:
+        case Sid::chordMinMajParentheses:
+        case Sid::chordAddOmitParentheses:
+        case Sid::automaticCapitalization:
+        case Sid::lowerCaseQualitySymbols:
         case Sid::chordDescriptionFile:
         case Sid::chordQualityMajorSeventh:
         case Sid::chordQualityHalfDiminished:
