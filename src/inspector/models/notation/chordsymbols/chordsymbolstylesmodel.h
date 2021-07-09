b34e765f5c16beedc1ce77fa130c0d875e41e066
Ashwath V A <vaashwath@gmail.com>
2021-07-09 19:00:54 +0530

    selection history expanded to work for All settings in the editor

--- src/inspector/models/notation/chordsymbols/chordsymbolstylesmodel.h
+++ src/inspector/models/notation/chordsymbols/chordsymbolstylesmodel.h
@@ -48,8 +48,12 @@ public:
     int currentStyleIndex() const;
 
     void initCurrentStyleIndex();
-    void updateQualitySymbols();
+    void setQualitySymbolsOnStyleChange();
+    void setPropertiesOnStyleChange();
+    void setChordSpelling(QString newSpelling);
     void extractSelectionHistory(QString selectionHistory);
+    void setStyleR(Ms::Sid id, qreal val);
+    void setStyleB(Ms::Sid id, bool val);
 
     Q_INVOKABLE void setChordStyle(QString styleName);
 
@@ -64,7 +68,9 @@ private:
 
     QList<Ms::ChordSymbolStyle> m_styles;
     ChordSymbolStyleManager* styleManager;
-    QHash<QString, QStringList> m_selectionHistory;
+    QHash<QString, QHash<QString, QVariant> > m_selectionHistory;
+
+    QStringList m_chordSpellingList;
 
     int m_currentStyleIndex;
 };
