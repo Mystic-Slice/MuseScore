6afb29746f1ebe4c6f4b961d946f6707dd89fcc4
Ashwath V A <vaashwath@gmail.com>
2021-07-16 15:48:30 +0530

    No preset style

--- src/engraving/libmscore/undo.cpp
+++ src/engraving/libmscore/undo.cpp
@@ -1819,6 +1819,7 @@ void ChangeStyleVal::flip(EditData*)
         case Sid::automaticCapitalization:
         case Sid::lowerCaseQualitySymbols:
         case Sid::chordDescriptionFile:
+        case Sid::useChordSymbolPresets:
         case Sid::chordQualityMajorSeventh:
         case Sid::chordQualityHalfDiminished:
         case Sid::chordQualityMinor:
