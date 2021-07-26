253e169496c460e52329661c9091431d873dcccb
Ashwath V A <vaashwath@gmail.com>
2021-07-26 23:02:07 +0530

    Bass note Options

--- src/engraving/libmscore/undo.cpp
+++ src/engraving/libmscore/undo.cpp
@@ -1830,6 +1830,7 @@ void ChangeStyleVal::flip(EditData*)
         case Sid::chordExtensionSixNine:
         case Sid::chordModifierOmit:
         case Sid::chordModifierSuspension:
+        case Sid::chordBassNote:
         case Sid::stackModifiers: {
             score->chordList()->unload();
             qreal qmag = score->styleD(Sid::chordQualityMag);
@@ -1845,6 +1846,7 @@ void ChangeStyleVal::flip(EditData*)
             score->chordList()->read(score->styleSt(Sid::chordDescriptionFile));
             score->chordList()->setCustomChordList(score->styleSt(Sid::chordStyle) == "custom");
             score->updateChordList();
+            score->chordList()->respellRenderListBase();
         }
         break;
         case Sid::spatium:
