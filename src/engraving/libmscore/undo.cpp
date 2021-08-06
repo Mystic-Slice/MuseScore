1b650e51fe9a6bbb8c7f55577544e1e189cb9931
Ashwath V A <vaashwath@gmail.com>
2021-08-06 20:07:41 +0530

    Separate toggles for capitalization of major and minor symbols

--- src/engraving/libmscore/undo.cpp
+++ src/engraving/libmscore/undo.cpp
@@ -1819,7 +1819,8 @@ void ChangeStyleVal::flip(EditData*)
         case Sid::chordMinMajParentheses:
         case Sid::chordAddOmitParentheses:
         case Sid::automaticCapitalization:
-        case Sid::lowerCaseQualitySymbols:
+        case Sid::lowerCaseMajorSymbols:
+        case Sid::lowerCaseMinorSymbols:
         case Sid::chordDescriptionFile:
         case Sid::useChordSymbolPresets:
         case Sid::chordQualityMajorSeventh:
